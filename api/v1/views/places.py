b41d5def2b71bf1cc37e1dd1598281e2bb9c0d48
sentayehut <tsentayehu4@gmail.com>
2022-04-18 11:21:36 -0700

    places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,107 +1,153 @@
 #!/usr/bin/python3
-"""
-    This is the places page handler for Flask.
-"""
+""" View for Place objects that handles default API actions """
 from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
 from models import storage
-from flask import abort, jsonify, request
-
 from models.city import City
 from models.place import Place
-from models.user import User
-from models.state import State
-from models.amenity import Amenity
+import requests
+import json
+from os import getenv
 
 
-@app_views.route('/cities/<id>/places', methods=['GET', 'POST'])
-def cities_id_places(id):
-    """
-        Flask route at /cities/<id>/places.
-    """
-    city = storage.get(City, id)
-    if (city):
-        if request.method == 'POST':
-            kwargs = request.get_json()
-            if not kwargs:
-                return {"error": "Not a JSON"}, 400
-            if "user_id" not in kwargs:
-                return {"error": "Missing user_id"}, 400
-
-            user = storage.get(User, kwargs.get("user_id", None))
-            if (user):
-                if "name" not in kwargs:
-                    return {"error": "Missing name"}, 400
-                new_place = Place(city_id=id, **kwargs)
-                new_place.save()
-                return new_place.to_dict(), 201
-
-        elif request.method == 'GET':
-            return jsonify([p.to_dict() for p in city.places])
-    abort(404)
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+def places(city_id):
+    """ Retrieves the list of all Place objects """
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    return jsonify([place.to_dict() for place in city.places])
 
 
-@app_views.route('/places/<id>', methods=['GET', 'DELETE', 'PUT'])
-def places_id(id):
-    """
-        Flask route at /places/<id>.
-    """
-    place = storage.get(Place, id)
-    if (place):
-        if request.method == 'DELETE':
-            place.delete()
-            storage.save()
-            return {}, 200
-
-        elif request.method == 'PUT':
-            kwargs = request.get_json()
-            if not kwargs:
-                return {"error": "Not a JSON"}, 400
-            for k, v in kwargs.items():
-                if k not in ["id", "user_id", "city_id",
-                             "created_at", "updated_at"]:
-                    setattr(place, k, v)
-            place.save()
-        return place.to_dict()
-    abort(404)
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def r_place_id(place_id):
+    """ Retrieves a Place object """
+    place = storage.get("Place", place_id)
+    if not place:
+        abort(404)
+    return jsonify(place.to_dict())
 
 
-@app_views.route('/places_search', methods=['POST'])
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_place(place_id):
+    """ Deletes a Place object """
+    place = storage.get("Place", place_id)
+    if not place:
+        abort(404)
+    place.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def post_place(city_id):
+    """ Creates a Place object """
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    new_place = request.get_json()
+    if not new_place:
+        abort(400, "Not a JSON")
+    if "user_id" not in new_place:
+        abort(400, "Missing user_id")
+    user_id = new_place['user_id']
+    if not storage.get("User", user_id):
+        abort(404)
+    if "name" not in new_place:
+        abort(400, "Missing name")
+    place = Place(**new_place)
+    setattr(place, 'city_id', city_id)
+    storage.new(place)
+    storage.save()
+    return make_response(jsonify(place.to_dict()), 201)
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_place(place_id):
+    """ Updates a Place object """
+    place = storage.get("Place", place_id)
+    if not place:
+        abort(404)
+
+    body_request = request.get_json()
+    if not body_request:
+        abort(400, "Not a JSON")
+
+    for k, v in body_request.items():
+        if k not in ['id', 'user_id', 'city_at',
+                     'created_at', 'updated_at']:
+            setattr(place, k, v)
+
+    storage.save()
+    return make_response(jsonify(place.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'],
+                 strict_slashes=False)
 def places_search():
     """
-        Flask route at /places_search
+    Retrieves all Place objects depending of
+    the JSON in the body of the request
     """
-    kwargs = request.get_json()
-    if not kwargs:
-        return {"error": "Not a JSON"}, 400
-    states = kwargs.get('states', [])
-    cities = kwargs.get('cities', [])
-    amenities = kwargs.get('amenities', [])
-    if states == cities == []:
-        places = storage.all("Place").values()
-    else:
-        places = []
-        for state_id in states:
-            state = storage.get(State, state_id)
-            for city in state.cities:
-                state_cities = state.cities
-                if city.id not in cities:
-                    cities.append(city.id)
-        for city_id in cities:
-            city = storage.get(City, city_id)
-            for place in city.places:
-                places.append(place)
-    search_result = []
-    amenity_objs = []
-    for amenity_id in amenities:
-        amenity = storage.get(Amenity, amenity_id)
-        if amenity:
-            amenity_objs.append(amenity)
-    for place in places:
-        amenities_cnt = 0
-        for amenity in amenity_objs:
-            if amenity not in place.amenities:
-                amenities_cnt += 1
-        if amenities_cnt == 0:
-            search_result.append(place.to_dict())
+    body_r = request.get_json()
+    if body_r is None:
+        abort(400, "Not a JSON")
 
-    return jsonify(search_result)
+    if not body_r or (
+            not body_r.get('states') and
+            not body_r.get('cities') and
+            not body_r.get('amenities')
+    ):
+        places = storage.all(Place)
+        return jsonify([place.to_dict() for place in places.values()])
+
+    places = []
+
+    if body_r.get('states'):
+        states = [storage.get("State", id) for id in body_r.get('states')]
+
+        for state in states:
+            for city in state.cities:
+                for place in city.places:
+                    places.append(place)
+
+    if body_r.get('cities'):
+        cities = [storage.get("City", id) for id in body_r.get('cities')]
+
+        for city in cities:
+            for place in city.places:
+                if place not in places:
+                    places.append(place)
+
+    if not places:
+        places = storage.all(Place)
+        places = [place for place in places.values()]
+
+    if body_r.get('amenities'):
+        ams = [storage.get("Amenity", id) for id in body_r.get('amenities')]
+        i = 0
+        limit = len(places)
+        HBNB_API_HOST = getenv('HBNB_API_HOST')
+        HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+        port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+        first_url = "http://0.0.0.0:{}/api/v1/places/".format(port)
+        while i < limit:
+            place = places[i]
+            url = first_url + '{}/amenities'
+            req = url.format(place.id)
+            response = requests.get(req)
+            am_d = json.loads(response.text)
+            amenities = [storage.get("Amenity", o['id']) for o in am_d]
+            for amenity in ams:
+                if amenity not in amenities:
+                    places.pop(i)
+                    i -= 1
+                    limit -= 1
+                    break
+            i += 1
+    return jsonify([place.to_dict() for place in places])
